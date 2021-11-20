4bde0d273c52e0137457ec2e2f1643add51584cb
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-20 16:45:27 +0100

    get all data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+	const [ users, setUsers ] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+      const response = await fetch('http://localhost:3022')
+			const users = await response.json();
+			setUsers(users);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>User Management App</h1>
+			<section className="users">
+				<ul>
+					{users.map((user, index) => {
+						return <li key={index}>{user.name}</li>;
+					})}
+				</ul>
+			</section>
+		</div>
+	);
 }
 
 export default App;
