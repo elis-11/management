aa5b8c93d3ab332429ca19911f0e980a2647c655
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-20 16:59:20 +0100

    added react-icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
+import { GrEdit } from 'react-icons/gr'
+import { RiDeleteBin6Line } from 'react-icons/ri'
 
 function App() {
 	const [ users, setUsers ] = useState([]);
 
 	useEffect(() => {
 		(async () => {
-      const response = await fetch('http://localhost:3022')
+			const response = await fetch('http://localhost:3022');
 			const users = await response.json();
 			setUsers(users);
 		})();
@@ -16,12 +18,33 @@ function App() {
 		<div className="App">
 			<h1>User Management App</h1>
 			<section className="users">
-				<ul>
-					{users.map((user, index) => {
-						return <li key={index}>{user.name}</li>;
-					})}
-				</ul>
-			</section>
+				{users.map((user, index) => {
+					return (
+						<div key={index} className="userCard">
+							<div className="row">
+								<div className="label">Full Name:</div>
+								<div className="data">{user.name}</div>
+							</div>
+							<div className="row">
+								<div className="label">User Name:</div>
+								<div className="data">{user.username}</div>
+							</div>
+							<div className="row">
+								<div className="label">E-Mail:</div>
+								<div className="data">{user.email}</div>
+							</div>
+							<div className="iconRow">
+								<button className="icon">
+									<RiDeleteBin6Line />
+								</button>
+								<button className="icon">
+									<GrEdit />
+								</button>
+							</div>
+						</div>
+					);
+				})}
+			</section>{' '}
 		</div>
 	);
 }
