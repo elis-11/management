b142df1b17b4fe2e89bf4111f82e0fefe259b5e5
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-20 18:21:42 +0100

    edit-button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.scss';
 import { GrEdit } from 'react-icons/gr';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
+const backendUrl = 'http://localhost:3022';
+
 function App() {
 	const [ users, setUsers ] = useState([]);
 
-	const backendUrl = 'http://localhost:3022';
 
 	const loadUsers = async () => {
 		const response = await fetch(backendUrl);
 		const users = await response.json();
+		users.forEach((user) => (user.isEditingEmail = false));
 		setUsers(users);
 	};
 
@@ -20,7 +22,32 @@ function App() {
 
 	const handleDeleteButton = async (user) => {
 		await fetch(`${backendUrl}/deleteuser/${user._id}`, {
-			method: 'DELETE'
+			method: 'DELETE'});
+		loadUsers();
+	};
+
+	const handleEditButton = (user) => {
+		user.isEditingEmail = !user.isEditingEmail;
+		setUsers([ ...users ]);
+	};
+
+  const handleEditCancelButton=(user)=>{
+    user.isEditingEmail = !user.isEditingEmail;
+    loadUsers();
+  }
+
+	const handleEmailChange = (user, e) => {
+		user.email = e.target.value;
+		setUsers([ ...users ]);
+	};
+
+	const handleEmailSave = async (user) => {
+		await fetch(`${backendUrl}/edituser/${user._id}`, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				email: user.email
+			})
 		});
 		loadUsers();
 	};
@@ -45,20 +72,33 @@ function App() {
 							</div>
 							<div className="row">
 								<div className="label">E-Mail:</div>
-								<div className="data">{user.email}</div>
+								{!user.isEditingEmail &&(
+                 <div className="data">{user.email}</div>)}
+								{user.isEditingEmail && (
+									<div className="data 
+                  editing"><input type="text"
+											onChange={(e) => handleEmailChange
+                        (user, e)} value={user.email}
+										/><button onClick={() => 
+                      handleEmailSave(user)}>Save</
+                      button><button onClick={() => 
+                      handleEditCancelButton(user)}>
+                        Cancel</button></div>
+								)}
 							</div>
 							<div className="iconRow">
-								<button onClick={() => handleDeleteButton(user)} className="icon">
-									<RiDeleteBin6Line />
+								<button onClick={() => 
+                  handleDeleteButton(user)} 
+                  className="icon"><RiDeleteBin6Line />
 								</button>
-								<button className="icon">
-									<GrEdit />
+								<button className="icon" onClick={() => 
+                  handleEditButton(user)}><GrEdit />
 								</button>
 							</div>
 						</div>
 					);
 				})}
-			</section>{' '}
+			</section>
 		</div>
 	);
 }
