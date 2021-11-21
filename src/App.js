ba1539df06f723b88e0db593544d5b698ba4a88b
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-21 11:43:52 +0100

    change App.js

--- src/App.js
+++ src/App.js
@@ -4,53 +4,51 @@ import { GrEdit } from 'react-icons/gr';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
 function App() {
-	const [ users, setUsers ] = useState([]);
+	const [users, setUsers] = useState([]);
 
-	const backendUrl = 'http://localhost:3022';
+	const backendUrl = 'http://localhost:3022'
 
 	const loadUsers = async () => {
 		const response = await fetch(backendUrl);
 		const users = await response.json();
-		users.forEach((user) => (user.isEditingEmail = false));
+		users.forEach(user => user.isEditingEmail = false);
 		setUsers(users);
-	};
+	}
 
 	useEffect(() => {
 		loadUsers();
-	});
+	}, []);
 
 	const handleDeleteButton = async (user) => {
-		await fetch(`${backendUrl}/deleteuser/${user._id}`, {
-			method: 'DELETE'
-		});
+		await fetch(`${backendUrl}/deleteuser/${user._id}`, { method: 'DELETE' });
 		loadUsers();
-	};
+	}
 
 	const handleEditButton = (user) => {
 		user.isEditingEmail = !user.isEditingEmail;
-		setUsers([ ...users ]);
-	};
+		setUsers([...users]);
+	}
 
 	const handleEditCancelButton = (user) => {
 		// user.isEditingEmail = !user.isEditingEmail;
 		loadUsers();
-	};
+	}
 
 	const handleEmailChange = (user, e) => {
 		user.email = e.target.value;
-		setUsers([ ...users ]);
-	};
+		setUsers([...users]);
+	}
 
 	const handleEmailSave = async (user) => {
-		await fetch(`${backendUrl}/edituser/${user._id}`, {
-			method: 'PATCH',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				email: user.email
-			})
-		});
-		loadUsers();
-	};
+			await fetch(`${backendUrl}/edituser/${user._id}`, {
+				method: 'PATCH',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					email: user.email
+				})
+			});
+			loadUsers();
+	}
 
 	return (
 		<div className="App">
@@ -70,32 +68,25 @@ function App() {
 								<div className="label">User Name:</div>
 								<div className="data">{user.username}</div>
 							</div>
-
 							<div className="row">
 								<div className="label">E-Mail:</div>
 								{!user.isEditingEmail && (
-									 <div className="data">{user.email}</div>)}
-									 {user.isEditingEmail && (
-									 <div className="data editing"><input type="text" onChange={(e) => handleEmailChange(user, e)}	value={user.email}/>
-										<button onClick={() => handleEmailSave(user)}>Save</button>
-										<button onClick={() => handleEditCancelButton(user)}>Cancel</button>
-									</div>
+									<div className="data">{user.email}</div>
+								)}
+								{user.isEditingEmail && (
+									<div className="data editing"><input type="text" onChange={(e) => handleEmailChange(user, e)} value={user.email} /><button onClick={() => handleEmailSave(user)}>Save</button><button onClick={() => handleEditCancelButton(user)}>Cancel</button></div>
 								)}
 							</div>
 							<div className="iconRow">
-								<button onClick={() => handleDeleteButton(user)} className="icon">
-									<RiDeleteBin6Line />
-								</button>
-								<button className="icon" onClick={() => handleEditButton(user)}>
-									<GrEdit />
-								</button>
+								<button onClick={() => handleDeleteButton(user)} className="icon"><RiDeleteBin6Line /></button>
+								<button className="icon" onClick={() => handleEditButton(user)}><GrEdit /></button>
 							</div>
 						</div>
-					);
+					)
 				})}
 			</section>
 		</div>
 	);
 }
 
-export default App;
+export default App; 
